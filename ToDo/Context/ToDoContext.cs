2d5e090957619f9c744f74e9bd0ef5e2405d73d4
lucas.arrabal_uscson <lucas.arrabal_uscson@PC-06-30>
2022-11-16 10:27:02 -0300

    Adicionar arquivos de projeto.

---
+++ ToDo/Context/ToDoContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDo.Models;
+
+namespace ToDo.Context
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Tarefa> Tarefas { get; set; }
+    }
+}
---
+++ ToDo/Models/EnumStatusTarefa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDo.Models
+{
+    public enum EnumStatusTarefa
+    {
+        Pendente,
+        Finalizado
+    }
+}
